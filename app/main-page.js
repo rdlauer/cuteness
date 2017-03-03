45e99f5e115ff7b3a84f482ac4a1af68a8dc0e35
Rob Lauer <rdlauer@gmail.com>
2017-03-03 09:10:42 -0600

    adding perfmon plugin

--- app/main-page.js
+++ app/main-page.js
@@ -1,6 +1,25 @@
 var frames = require("ui/frame");
 var redditAppViewModel = require("./reddit-app-view-model");
 var appViewModel = new redditAppViewModel.AppViewModel();
+
+var perfMon = require("nativescript-performance-monitor");
+var color = require("color");
+
+var performanceMonitor = new perfMon.PerformanceMonitor();
+
+performanceMonitor.start({
+  textColor: new color.Color("white"),
+  backgroundColor: new color.Color("black"),
+  borderColor: new color.Color("black"),
+  hide: false,
+  onSample: function (sample) {
+    console.log("FPS: " + sample.fps);
+    if (sample.cpu) { // iOS only
+      console.log("CPU %: " + sample.cpu);
+    }
+  }
+});
+
 function pageLoaded(args) {
     var page = args.object;
     page.bindingContext = appViewModel;
